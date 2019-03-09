fff70deb31dceec1071e84081fe42ca3276be1a1
Punit Suman <punitsuman89@gmail.com>
2019-03-09 21:38:42 +0530

    Added tag option in runner file.

--- CucumberBDDFramework/test_output/report.js
+++ CucumberBDDFramework/test_output/report.js
@@ -13,19 +13,19 @@ formatter.feature({
   ]
 });
 formatter.before({
-  "duration": 657762,
+  "duration": 292961,
   "status": "passed"
 });
 formatter.before({
-  "duration": 472563,
+  "duration": 136683,
   "status": "passed"
 });
 formatter.before({
-  "duration": 164674,
+  "duration": 55513,
   "status": "passed"
 });
 formatter.before({
-  "duration": 155343,
+  "duration": 55980,
   "status": "passed"
 });
 formatter.scenario({
@@ -66,21 +66,21 @@ formatter.match({
   "location": "MyStepDef2PS.login_to_application()"
 });
 formatter.result({
-  "duration": 135744382,
+  "duration": 142245964,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepDef2PS.search_any_claim()"
 });
 formatter.result({
-  "duration": 69041,
+  "duration": 66243,
   "status": "passed"
 });
 formatter.match({
   "location": "MyStepDef2PS.user_should_be_navigate_to_View_Claim_Documents_page()"
 });
 formatter.result({
-  "duration": 60645,
+  "duration": 176803,
   "status": "passed"
 });
 formatter.match({
@@ -97,261 +97,23 @@ formatter.match({
   "location": "MyStepDef2PS.login_to_application_with_user_and(String,String)"
 });
 formatter.result({
-  "duration": 2806918,
+  "duration": 3259422,
   "status": "passed"
 });
 formatter.after({
-  "duration": 65310,
+  "duration": 208992,
   "status": "passed"
 });
 formatter.after({
-  "duration": 57380,
+  "duration": 187066,
   "status": "passed"
 });
 formatter.after({
-  "duration": 78838,
+  "duration": 77439,
   "status": "passed"
 });
 formatter.after({
-  "duration": 73241,
-  "status": "passed"
-});
-formatter.scenarioOutline({
-  "comments": [
-    {
-      "line": 11,
-      "value": "# this line and below outline scenario line will call same step definition having 2 String arguments."
-    }
-  ],
-  "line": 15,
-  "name": "scenario outline with examples",
-  "description": "",
-  "id": "my-second-feature-file;scenario-outline-with-examples",
-  "type": "scenario_outline",
-  "keyword": "Scenario Outline"
-});
-formatter.step({
-  "line": 17,
-  "name": "Login to application with user \"\u003cusername\u003e\" and \"\u003cpassword\u003e\"",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 18,
-  "name": "Search any claim",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 19,
-  "name": "User should be navigate to View Claim Documents page",
-  "keyword": "Then "
-});
-formatter.examples({
-  "line": 20,
-  "name": "",
-  "description": "",
-  "id": "my-second-feature-file;scenario-outline-with-examples;",
-  "rows": [
-    {
-      "cells": [
-        "username",
-        "password"
-      ],
-      "line": 21,
-      "id": "my-second-feature-file;scenario-outline-with-examples;;1"
-    },
-    {
-      "cells": [
-        "naveenk",
-        "test@123"
-      ],
-      "line": 22,
-      "id": "my-second-feature-file;scenario-outline-with-examples;;2"
-    },
-    {
-      "cells": [
-        "punit",
-        "temp@456"
-      ],
-      "line": 23,
-      "id": "my-second-feature-file;scenario-outline-with-examples;;3"
-    }
-  ],
-  "keyword": "Examples"
-});
-formatter.before({
-  "duration": 149745,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 91900,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 49915,
-  "status": "passed"
-});
-formatter.scenario({
-  "line": 22,
-  "name": "scenario outline with examples",
-  "description": "",
-  "id": "my-second-feature-file;scenario-outline-with-examples;;2",
-  "type": "scenario",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "line": 1,
-      "name": "@First"
-    }
-  ]
-});
-formatter.step({
-  "line": 17,
-  "name": "Login to application with user \"naveenk\" and \"test@123\"",
-  "matchedColumns": [
-    0,
-    1
-  ],
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 18,
-  "name": "Search any claim",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 19,
-  "name": "User should be navigate to View Claim Documents page",
-  "keyword": "Then "
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "naveenk",
-      "offset": 32
-    },
-    {
-      "val": "test@123",
-      "offset": 46
-    }
-  ],
-  "location": "MyStepDef2PS.login_to_application_with_user_and(String,String)"
-});
-formatter.result({
-  "duration": 193596,
-  "status": "passed"
-});
-formatter.match({
-  "location": "MyStepDef2PS.search_any_claim()"
-});
-formatter.result({
-  "duration": 61577,
-  "status": "passed"
-});
-formatter.match({
-  "location": "MyStepDef2PS.user_should_be_navigate_to_View_Claim_Documents_page()"
-});
-formatter.result({
-  "duration": 54580,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 51315,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 74640,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 69508,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 146480,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 92833,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 55514,
-  "status": "passed"
-});
-formatter.scenario({
-  "line": 23,
-  "name": "scenario outline with examples",
-  "description": "",
-  "id": "my-second-feature-file;scenario-outline-with-examples;;3",
-  "type": "scenario",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "line": 1,
-      "name": "@First"
-    }
-  ]
-});
-formatter.step({
-  "line": 17,
-  "name": "Login to application with user \"punit\" and \"temp@456\"",
-  "matchedColumns": [
-    0,
-    1
-  ],
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 18,
-  "name": "Search any claim",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 19,
-  "name": "User should be navigate to View Claim Documents page",
-  "keyword": "Then "
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "punit",
-      "offset": 32
-    },
-    {
-      "val": "temp@456",
-      "offset": 44
-    }
-  ],
-  "location": "MyStepDef2PS.login_to_application_with_user_and(String,String)"
-});
-formatter.result({
-  "duration": 190798,
-  "status": "passed"
-});
-formatter.match({
-  "location": "MyStepDef2PS.search_any_claim()"
-});
-formatter.result({
-  "duration": 58312,
-  "status": "passed"
-});
-formatter.match({
-  "location": "MyStepDef2PS.user_should_be_navigate_to_View_Claim_Documents_page()"
-});
-formatter.result({
-  "duration": 54114,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 49916,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 79772,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 86768,
+  "duration": 74173,
   "status": "passed"
 });
 });
